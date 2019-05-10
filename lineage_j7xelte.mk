ecba0138213caca9299634fc4e64ed4bab6a6155
yEsPaP <46491239+yEsPaP@users.noreply.github.com>
2019-05-10 15:24:38 +0300

    Update lineage_j7xelte.mk

--- lineage_j7xelte.mk
+++ lineage_j7xelte.mk
@@ -23,8 +23,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-WITH_SU := true
-
 # Device identifier
 PRODUCT_NAME := lineage_j7xelte
 PRODUCT_DEVICE := j7xelte
